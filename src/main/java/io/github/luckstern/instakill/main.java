16c15df32de55a143223c9d000a4184b01d5ef05
Luckstern <rajghole@icloud.com>
2021-05-16 20:03:39 -0400

    Made actually working code so players can now one-hit any entity.

--- src/main/java/io/github/luckstern/instakill/main.java
+++ src/main/java/io/github/luckstern/instakill/main.java
@@ -1,27 +1,29 @@
 package io.github.luckstern.instakill;
 
-import org.bukkit.Bukkit;
-import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class main extends JavaPlugin implements Listener {
+
+    @EventHandler
+    public void onEntityDamage(EntityDamageByEntityEvent event) {
+        if (event.getDamager() instanceof Player) {
+            ((LivingEntity) event.getEntity()).setHealth(0D);
+        }
+    }
+
     @Override
     public void onEnable() {
         getLogger().info("Instakill has officially been enabled!");
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
         getLogger().info("Instakill has officially been disabled!");
-    }
-
-    @EventHandler
-    public void onEntityDamage(EntityDamageByEntityEvent event) {
-        if (event.getDamager().getType().equals(EntityType.PLAYER)) {
-            Bukkit.broadcastMessage("Entity has been hit by player!");
-        }
     }
 }
